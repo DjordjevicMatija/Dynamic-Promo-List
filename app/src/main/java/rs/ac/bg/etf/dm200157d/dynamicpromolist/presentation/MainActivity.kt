85ac71280c5e327c0695627a3319e9cad47cf603
Matija Djordjevic <matija.djordjevic@rt-rk.com>
2024-08-13 12:13:16 +0200

    Implemented getMovies in ViewModel
    Change-Id: I2f17e0aa468da3412d18c284a5ac246106a56587

--- app/src/main/java/rs/ac/bg/etf/dm200157d/dynamicpromolist/presentation/MainActivity.kt
+++ app/src/main/java/rs/ac/bg/etf/dm200157d/dynamicpromolist/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package rs.ac.bg.etf.dm200157d.dynamicpromolist.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
 import rs.ac.bg.etf.dm200157d.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mainViewModel.moviesLiveData.observe(this){movieList ->
+            Log.d("MainActivity", "Movies: $movieList")
+        }
+
+        mainViewModel.errorLiveData.observe(this){error ->
+            Log.e("MainActivity", "Error: ${error.message}")
+        }
+
+        mainViewModel.getMovies()
     }
 }
--- app/src/main/java/rs/ac/bg/etf/dm200157d/dynamicpromolist/presentation/MainViewModel.kt
+++ app/src/main/java/rs/ac/bg/etf/dm200157d/dynamicpromolist/presentation/MainViewModel.kt
@@ -1,4 +1,33 @@
 package rs.ac.bg.etf.dm200157d.dynamicpromolist.presentation
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import rs.ac.bg.etf.dm200157d.dynamicpromolist.domain.DataResult
+import rs.ac.bg.etf.dm200157d.dynamicpromolist.domain.UseCase
+import rs.ac.bg.etf.dm200157d.dynamicpromolist.domain.entities.MovieList
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val useCase: UseCase
+) : ViewModel() {
+
+    private val _moviesLiveData = MutableLiveData<MovieList>()
+    val moviesLiveData: LiveData<MovieList> get() = _moviesLiveData
+
+    private val _errorLiveData = MutableLiveData<Exception>()
+    val errorLiveData: LiveData<Exception> get() = _errorLiveData
+
+    fun getMovies() {
+        viewModelScope.launch {
+            when (val result = useCase.getMovies()) {
+                is DataResult.Success -> _moviesLiveData.postValue(result.data)
+                is DataResult.Failure -> _errorLiveData.postValue(result.error)
+            }
+        }
+    }
 }
